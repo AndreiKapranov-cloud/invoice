12e8831cb3b093219b91ecff4e3ddd8c01de5228
AndreiKapranov-cloud <akapranov872@gmail.com>
2022-03-22 12:40:41 +0300

    22.03

--- force-app/main/default/lwc/scheduleBatch/scheduleBatch.js
+++ force-app/main/default/lwc/scheduleBatch/scheduleBatch.js
@@ -1,3 +1,34 @@
-import { LightningElement } from 'lwc';
 
-export default class ScheduleBatch extends LightningElement {}
+import { LightningElement, api, wire, track } from 'lwc';
+import executeBatchOneTime from '@salesforce/apex/BirthdayBatch.executeBatchOneTime';
+import abortBatch from '@salesforce/apex/BirthdayBatch.abortBatch';
+import scheduleBatch from '@salesforce/apex/SchedBatch.scheduleBatch';
+export default class ScheduleBatch extends LightningElement {
+    
+    
+    @track crron;
+    
+    batchScheduled = false;
+    
+    
+    executeBatchOneTimeHandler(event) {
+ 
+        executeBatchOneTime();
+    }
+    handleCronInput(event) {
+        if (event.target.name === 'schdlbtch') {   
+        this.crron = event.target.value;
+     }
+    }
+    scheduleBatchHandler(event) {
+        
+        scheduleBatch({cron:this.crron});
+        this.batchScheduled = true;
+    }
+    abortBatchHandler(){
+        
+        abortBatch();
+        this.batchScheduled = false;
+    }
+
+}
