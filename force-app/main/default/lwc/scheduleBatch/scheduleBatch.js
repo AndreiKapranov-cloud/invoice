844004eaabf0ef45de154c6a888805b5b4e294bf
AndreiKapranov-cloud <akapranov872@gmail.com>
2022-03-22 21:09:46 +0300

    220322

--- force-app/main/default/lwc/scheduleBatch/scheduleBatch.js
+++ force-app/main/default/lwc/scheduleBatch/scheduleBatch.js
@@ -2,12 +2,16 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import executeBatchOneTime from '@salesforce/apex/BirthdayBatch.executeBatchOneTime';
 import abortBatch from '@salesforce/apex/BirthdayBatch.abortBatch';
-import scheduleBatch from '@salesforce/apex/SchedBatch.scheduleBatch';
+import scheduleBatch from '@salesforce/apex/Crossroad.scheduleBatch';
 export default class ScheduleBatch extends LightningElement {
+    @api schedulableClassName;
+    @api batchableClassName;
+    @api title; 
+    @api descriptionText;
     
-    
-    @track crron;
-    
+    @track cron;
+    @track disableBtn = true;
+
     batchScheduled = false;
     
     
@@ -17,18 +21,24 @@ export default class ScheduleBatch extends LightningElement {
     }
     handleCronInput(event) {
         if (event.target.name === 'schdlbtch') {   
-        this.crron = event.target.value;
+        this.cron = event.target.value;
+        if(this.cron == null || this.cron == ''||this.cron.length < 11){
+            this.disableBtn = true;
+          }else{
+            this.disableBtn = false;
+          }
      }
     }
     scheduleBatchHandler(event) {
         
-        scheduleBatch({cron:this.crron});
+        scheduleBatch({cron:this.cron});
         this.batchScheduled = true;
     }
     abortBatchHandler(){
         
         abortBatch();
         this.batchScheduled = false;
+        this.disableBtn = true;
     }
 
 }
